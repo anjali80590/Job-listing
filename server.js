f597d5ec47e3340506f2b9d43e6844a762fed6b8
Anjali <112229496+anjali80590@users.noreply.github.com>
2023-12-08 13:08:32 +0530

    added database config and  and initialization

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 // server.js
 const express = require('express');
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+ 
+
 
 app.get('/', (req, res) => {
   res.send('Hello, this is your server!');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'UP' });
+  });
+
+
+
+
+
+app.listen(process.env.PORT , () => {
+    mongoose 
+    .connect(process.env.DB_URL)
+    .then(()=> console.log(`Server is running on http://localhost:${process.env.PORT }`))
+    .catch((err)=>console.log(err))
+ 
 });
