2c097eb3dd64aa279fc5bd0e14b8ffdbba4ea647
Anjali <112229496+anjali80590@users.noreply.github.com>
2023-12-09 11:46:34 +0530

    added database model

---
+++ model.js
@@ -0,0 +1,11 @@
+// models.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = { UserModel };
--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 // server.js
-const express = require('express');
-const mongoose=require('mongoose')
-const dotenv=require('dotenv')
-dotenv.config()
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
 
- 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello, this is your server!');
+app.get("/", (req, res) => {
+  res.send("Hello, this is your server!");
 });
 
+app.get("/health", (req, res) => {
+  res.json({ status: "UP" });
+});
 
-app.get('/health', (req, res) => {
-    res.json({ status: 'UP' });
-  });
-
-
-
-
-
-app.listen(process.env.PORT , () => {
-    mongoose 
+app.listen(process.env.PORT, () => {
+  mongoose
     .connect(process.env.DB_URL)
-    .then(()=> console.log(`Server is running on http://localhost:${process.env.PORT }`))
-    .catch((err)=>console.log(err))
- 
+    .then(() =>
+      console.log(`Server is running on http://localhost:${process.env.PORT}`)
+    )
+    .catch((err) => console.log(err));
 });
